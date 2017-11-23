46e6c62860e203b6de35c6b6c576e27ede23d920
JULIOCANCHALA <juliofernando1c@hotmail.com>
2017-11-23 14:11:35 -0500

    Ready header style and imgname

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function compress_menu(item) {
+    item.style.height = "26px";
+    console.log("1");
+}
+
+function decompress_menu(item) {
+    item.style.height = "30px";
+    console.log("0");
+}
